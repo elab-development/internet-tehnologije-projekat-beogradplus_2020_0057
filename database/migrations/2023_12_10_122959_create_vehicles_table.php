a2c46b5ebd6fb12fb8f6a9a230d5c2b7c2ee356a
Filip Kastratovic <kastro.filip@gmail.com>
2023-12-10 13:58:36 +0100

    Dodat model Vehicle

---
+++ database/migrations/2023_12_10_122959_create_vehicles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('trenutna_stanica')->constrained('stops');
+            $table->foreignId('linija')->constrained('lines');
+            $table->foreignId('tip')->constrained('vehicle_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
