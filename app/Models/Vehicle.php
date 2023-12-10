731eb6b3c60cf4274e1c50e0bdd08903e908a41b
Milos Lekic <lekicm2019@gmail.com>
2023-12-10 16:29:25 +0100

    dodato hasmany

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Vehicle extends Model
 {
     use HasFactory;
+
+    public function current_stop() {
+        return $this->hasMany(Stop::class, 'trenutna_stanica');
+    }
+
+    public function line() {
+        return $this->hasMany(Line::class, 'linija');
+    }
 }
